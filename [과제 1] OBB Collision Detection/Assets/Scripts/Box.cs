f3b6937c41a84c1d65f80d4a6c930fb6f440f9e7
mob954325 <lsh954325@gmail.com>
2025-04-01 13:06:44 +0900

    라인 랜더러 추가중

---
+++ [과제 1] OBB Collision Detection/Assets/Scripts/Box.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Box : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    Vector2 dirVec = Vector2.zero;
+    float speed = 20f;
+    float rotSpeed = 180f;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        dirVec = Random.insideUnitCircle;
+
+        // lineRenderer
+        lineRenderer.loop = true;
+        lineRenderer.positionCount = 4;
+    }
+
+    void Update()
+    {
+        CheckBoarder();
+        transform.Translate(dirVec * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+
+        //lineRenderer.SetPosition(0, );
+        //lineRenderer.SetPosition(1, );
+        //lineRenderer.SetPosition(2, );
+        //lineRenderer.SetPosition(3, );
+    }
+
+    void CheckBoarder()
+    {
+        Vector3 viewportVec = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (viewportVec.x < 0 || viewportVec.x > 1) dirVec.x *= -1;
+        if (viewportVec.y < 0 || viewportVec.y > 1) dirVec.y *= -1;
+    }
+}
---
+++ [과제 1] OBB Collision Detection/Assets/Scripts/BoxManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxManager : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public int boxCount = 100;
+    // 박스 생성
+    // 박스 충돌 확인
+
+    private void Start()
+    {
+        for(int i = 0; i < boxCount; i++)
+        {
+            Instantiate(boxPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}

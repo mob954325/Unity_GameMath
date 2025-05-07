a98e9c351181c12efd210b9c06d88194d9a3ec40
mob954325 <lsh954325@gmail.com>
2025-05-07 15:13:06 +0900

    투사체 로직 테스트 중

--- GameMath1Q/Assets/Scripts/Scene1/ProjectileBar.cs
+++ GameMath1Q/Assets/Scripts/Scene1/ProjectileBar.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ProjectileBar : MonoBehaviour
 {
     Camera mainCam;
+    Transform attackImagePivot;
+    Transform warningImagePivot;
 
+    public float maxViewportValueX; // 이 투사체 목표로하는 x좌표값
     // 스폰 이후 행동
     // 시작 후 선형 보간으로 최종 이동할 위치를 WarningImagePivot을 늘려서 알려줌
     // 2초 뒤 WarningImage는 없어지고 AttackImage가 해당 위치까지 늘어남
@@ -13,11 +16,39 @@ public class ProjectileBar : MonoBehaviour
 
     private void Awake()
     {
-        
+        mainCam = Camera.main;
+        attackImagePivot = transform.GetChild(0);
+        warningImagePivot = transform.GetChild(1);        
     }
 
-    private float SetMaxSize()
+    private void Start()
     {
-        return 1f;
+        maxViewportValueX = UnityEngine.Random.value; // 0.0 - 1.0
+        StartCoroutine(AttackProcess());
+    }
+
+    private IEnumerator AttackProcess()
+    {
+        Vector2 viewportPosition = mainCam.WorldToViewportPoint(transform.position);
+        viewportPosition.x += maxViewportValueX;
+        Vector2 worldPosition = mainCam.ViewportToWorldPoint(viewportPosition);
+
+        float timeElapsed = 0.0f;
+        while(timeElapsed < 1.0f)
+        {
+            timeElapsed += Time.deltaTime;
+            Vector3 localScale = warningImagePivot.localScale;
+            float x = Mathf.Lerp(localScale.x, worldPosition.x, timeElapsed);
+            warningImagePivot.localScale = new Vector3(x, warningImagePivot.localScale.y, warningImagePivot.localScale.z);
+            yield return null;
+        }
+        warningImagePivot.localScale = new Vector3(viewportPosition.x, warningImagePivot.localScale.y, warningImagePivot.localScale.z);
+
+        yield return new WaitForSeconds(2f);
+
+        attackImagePivot.localScale = new Vector3(viewportPosition.x, warningImagePivot.localScale.y, warningImagePivot.localScale.z);
+
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
     }
 }
--- GameMath1Q/Assets/Scripts/Test/Test_00_Projectile.cs
+++ GameMath1Q/Assets/Scripts/Test/Test_00_Projectile.cs
@@ -7,10 +7,17 @@ public class Test_00_Projectile : TestBase
 {
     public GameObject spawnPoint;
     public GameObject projectileSin;
+    public GameObject projectileBar;
 
     protected override void OnTest1(InputAction.CallbackContext context)
     {
         Debug.Log("1: Spawn projectileSin");
         Instantiate(projectileSin, spawnPoint.transform.position, Quaternion.identity);
+    }
+
+    protected override void OnTest2(InputAction.CallbackContext context)
+    {
+        Debug.Log("2: Spawn projectileBar");
+        Instantiate(projectileBar, spawnPoint.transform.position, Quaternion.identity);
     }
 }
